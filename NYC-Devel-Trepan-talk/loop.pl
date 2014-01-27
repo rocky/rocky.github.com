c407daf70f5f806253a3905c1cdef2e059bbd93a
Rocky Bernstein <rocky@gnu.org>
2014-01-27 00:10:59 -0500

    Add location of NYC Stack slides

--- NYC-Devel-Trepan-talk/loop.pl
+++ NYC-Devel-Trepan-talk/loop.pl
@@ -1,10 +1,10 @@
 use Enbugger "trepan";
 $leave_loop = 0;
-$SIG{'HUP'} = sub {
-    my $sig = shift;
-    print "Got signal $sig in debugged program handler\n";
-    Enbugger->stop;
-};
+# $SIG{'HUP'} = sub {
+#     my $sig = shift;
+#     print "Got signal $sig in debugged program handler\n";
+#     Enbugger->stop;
+# };
 print "My pid is $$. Looping...\n";
 until ($leave_loop) {
     sleep 1;
